6e9b00e9c8eccdc7542e33739ec77c0153757b94
Nathan Cook <nathan@Nathans-MBP.client.easlab.co.uk>
2024-06-13 15:26:11 +0100

    k8s

--- utils/dbmigration.go
+++ utils/dbmigration.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	dbconfig "github.com/nathanjcook/discordbotgo/config"
 )
 
 func main() {
 	// Find .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %s", err)
+	// }
 	dbconfig.Connect()
 	dbconfig.DB.AutoMigrate()
 }
