2b252b7f6ac007871bd8262b45be473fe3bcfb2e
William Thomas <williamthomas@Williams-MBP.client.easlab.co.uk>
2024-06-14 12:15:42 +0100

    DeletecMD And ArgLength Validation

--- bot/bot.go
+++ bot/bot.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	addcommand "github.com/nathanjcook/discordbotgo/add_command"
+	deletecommand "github.com/nathanjcook/discordbotgo/delete_command"
 )
 
 var BotId string
@@ -44,7 +45,6 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	// if m.content contains botid (Mentions) and "ping" then send "pong!"
 	if strings.Contains(m.Content, os.Getenv("BOT_PREFIX")) {
 		cmdsplit := strings.Split(m.Content, " ")
 		fmt.Println(cmdsplit)
@@ -55,11 +55,28 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				_, _ = s.ChannelMessageSend(m.ChannelID, "Error Running Command")
 				panic(err)
 			}
-			if len(cmdsplit) < 5 {
+			if len(cmdsplit) < 5 || len(cmdsplit) > 6 {
 				_, _ = s.ChannelMessageSend(m.ChannelID, "Invalid Amount Of Args Provided")
 			} else {
 				if p&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator {
 					_, _ = s.ChannelMessageSend(m.ChannelID, addcommand.Add(cmdsplit[2], cmdsplit[3], cmdsplit[4]))
+				} else {
+					_, _ = s.ChannelMessageSend(m.ChannelID, "Only Admins Can Add MicroServices! Please Contact Any Administrators If You Want A Particular Microservice Added")
+				}
+			}
+		}
+
+		if cmdsplit[1] == "delete" {
+			p, err := s.UserChannelPermissions(m.Author.ID, m.ChannelID)
+			if err != nil {
+				_, _ = s.ChannelMessageSend(m.ChannelID, "Error Running Command")
+				panic(err)
+			}
+			if len(cmdsplit) < 3 || len(cmdsplit) > 4 {
+				_, _ = s.ChannelMessageSend(m.ChannelID, "Invalid Amount Of Args Provided")
+			} else {
+				if p&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator {
+					_, _ = s.ChannelMessageSend(m.ChannelID, deletecommand.Delete(cmdsplit[2]))
 				} else {
 					_, _ = s.ChannelMessageSend(m.ChannelID, "Only Admins Can Add MicroServices! Please Contact Any Administrators If You Want A Particular Microservice Added")
 				}
